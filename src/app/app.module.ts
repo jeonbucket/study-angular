712bc70e414f3dd7368ee4aee16f4868d4e6f01a
jeonbucket <jeonbucket@gmail.com>
2017-03-16 15:51:16 +0900

    Add LoginComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { UserFormComponent } from './user-form.component';
 import { LoginComponent } from './login.component';
+
+import { LoginService } from './login.service';
 
 @NgModule({
   declarations: [
@@ -16,9 +18,10 @@ import { LoginComponent } from './login.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [LoginService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
